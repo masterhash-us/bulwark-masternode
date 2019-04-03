5001da4b0ac57b0471236e12411f76b87c608d61
Penple <penple@penple.org>
2019-04-02 20:32:22 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ while [ "$KEY" == "" ]
 do
     KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "Bulwark Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
 done
+echo "masternode=1" >> ~/.bulwark/bulwark.conf
 echo "masternodeprivkey=$KEY" >> ~/.bulwark/bulwark.conf
 sudo service bulwarkd restart
 
