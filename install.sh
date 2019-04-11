a2f99d378277850c96c9064912645c893ea11893
Daniel <30388756+whywefight@users.noreply.github.com>
2019-04-11 03:35:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 while [ "$KEY" == "" ]
 do
-    KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "Bulwark Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
+    KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "GinCoin Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
 done
-echo "masternode=1" >> ~/.bulwark/bulwark.conf
-echo "masternodeprivkey=$KEY" >> ~/.bulwark/bulwark.conf
-sudo service bulwarkd restart
+echo "masternode=1" >> ~/.gincoincore/gincoin.conf
+echo "masternodeprivkey=$KEY" >> ~/.gincoincore/gincoin.conf
+sudo service gincoind restart
 
-until bulwark-cli getinfo >/dev/null; do
+until gincoin-cli getinfo >/dev/null; do
   sleep 1;
 done
