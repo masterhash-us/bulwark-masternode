c39e308f409b6052ae88d51568d5526e9883b6de
Daniel <30388756+whywefight@users.noreply.github.com>
2019-04-12 04:32:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
 function Status() {
-    if STATUS=$(bulwark-cli masternode status 2>&1); then
+    if STATUS=$(phore-cli masternode status 2>&1); then
         TXHASH=$(jq -r .txhash <<< "$STATUS")
         TXN=$(jq -r .outputidx <<< "$STATUS")
         TX="$TXHASH:$TXN"
         ADDRESS=$(jq -r .addr <<< "$STATUS")
         MESSAGE=$(jq -r .message <<< "$STATUS")
-        whiptail --title "Bulwark Masternode" --msgbox "TX: $TX\nAddress: $ADDRESS\nStatus: $MESSAGE" 10 78
+        whiptail --title "phore Masternode" --msgbox "TX: $TX\nAddress: $ADDRESS\nStatus: $MESSAGE" 10 78
     else
-        whiptail --title "Bulwark Masternode" --msgbox "Failed retriving masternode status.\n$STATUS" 10 78
+        whiptail --title "phore Masternode" --msgbox "Failed retriving masternode status.\n$STATUS" 10 78
     fi
 }
 
 function Restart() {
-    sudo service bulwarkd restart
-    until bulwark-cli getinfo >/dev/null; do
+    sudo service phored restart
+    until phore-cli getinfo >/dev/null; do
         sleep 1;
     done
 }
 
 function Refresh() {
-    sudo service bulwarkd stop
-    rm -rf ~/.bulwark/blocks ~/.bulwark/database ~/.bulwark/chainstate ~/.bulwark/peers.dat
-    sudo service bulwarkd start
-    until bulwark-cli getinfo >/dev/null; do
+    sudo service phored stop
+    rm -rf ~/.phore/blocks ~/.phore/database ~/.phore/chainstate ~/.phore/peers.dat
+    sudo service phored start
+    until phore-cli getinfo >/dev/null; do
         sleep 1;
     done
 }
@@ -40,7 +40,7 @@ function Shell() {
 }
 
 function Menu() {
-    SEL=$(whiptail --nocancel --title "Bulwark Masternode" --menu "Choose an option" 16 78 8 \
+    SEL=$(whiptail --nocancel --title "phore Masternode" --menu "Choose an option" 16 78 8 \
         "Status" "Display masternode status." \
         "Restart" "Restart masternode." \
         "Refresh" "Wipe and reinstall blockchain." \
@@ -56,7 +56,7 @@ function Menu() {
     esac
 }
 
-if ! grep -q "masternodeprivkey=" ~/.bulwark/bulwark.conf; then
+if ! grep -q "masternodeprivkey=" ~/.phore/phore.conf; then
     cd /opt/masternode
     sudo git pull
     bash /opt/masternode/install.sh
