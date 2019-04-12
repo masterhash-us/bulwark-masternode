3822ecca4c386fa6df18d458f325ede98a284fa8
Daniel <30388756+whywefight@users.noreply.github.com>
2019-04-12 02:51:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ while [ "$KEY" == "" ]
 do
     KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "Alqo Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
 done
-echo "masternode=1" >> ~/.bulwark/bulwark.conf
+echo "masternode=1" >> ~/.alqo/alqo.conf
 echo "masternodeprivkey=$KEY" >> ~/.alqo/alqo.conf
 sudo service alqod restart
 
