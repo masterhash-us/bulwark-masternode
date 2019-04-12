138afc40ad478fceb51665853d33b2e05e5b44cd
Daniel <30388756+whywefight@users.noreply.github.com>
2019-04-12 04:30:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 while [ "$KEY" == "" ]
 do
-    KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "Bulwark Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
+    KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "Phore Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
 done
-echo "masternode=1" >> ~/.bulwark/bulwark.conf
-echo "masternodeprivkey=$KEY" >> ~/.bulwark/bulwark.conf
-sudo service bulwarkd restart
+echo "masternode=1" >> ~/.phore/phore.conf
+echo "masternodeprivkey=$KEY" >> ~/.phore/phore.conf
+sudo service phored restart
 
-until bulwark-cli getinfo >/dev/null; do
+until phore-cli getinfo >/dev/null; do
   sleep 1;
 done
