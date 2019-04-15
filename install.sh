8e0ba27d98e16588e78c76a9554fd6cb2481e850
masterhash-us <32805608+masterhash-us@users.noreply.github.com>
2019-04-16 07:15:43 +1000

    RPC rand user and pass

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# Generate random passwords
+RPCUSER=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+RPCPASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
 while [ "$KEY" == "" ]
 do
     KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "Bulwark Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
 done
 echo "masternode=1" >> ~/.bulwark/bulwark.conf
 echo "masternodeprivkey=$KEY" >> ~/.bulwark/bulwark.conf
+echo "rpcpassword=${RPCPASSWORD}"  >> ~/.bulwark/bulwark.conf
+echo "rpcuser=${RPCUSER}"  >> ~/.bulwark/bulwark.conf
 sudo service bulwarkd restart
 
 until bulwark-cli getinfo >/dev/null; do
